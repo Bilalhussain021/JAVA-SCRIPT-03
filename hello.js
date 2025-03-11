63611f25fbd138e549e1e6a5b670cd4e2faf9baf
bilalhussain021 <bilal3june@gmail.com>
2025-03-12 03:51:18 +0500

    java script assignment 03

---
+++ hello.js
@@ -0,0 +1,26 @@
+// Task 01
+let age = 22;
+alert(`I am ${age} Year Old`);
+
+// Task 02
+let visitCount = 13;
+visitCount++;
+alert(`You have visited this site ${visitCount} times.`);
+
+// Task 03
+let myBirthYear = 2002;
+myBirthYear = Number(myBirthYear);
+document.write(
+  ` My birth year is ${myBirthYear}. \n Data type of my decleared variable is ` +
+    typeof myBirthYear +
+    "<br>" +
+    "<br>"
+);
+
+// Task 04
+let visitorName = "John Doe";
+let productTitle = "T-shirt";
+let quantity = 5;
+document.write(
+  visitorName + "ordered " + quantity + productTitle + " on XYZ Clothing store"
+);
